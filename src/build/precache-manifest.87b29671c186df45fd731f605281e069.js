a71991e51f45273706251f57a791638fb50c58ae
Yingxi Lin <yxlin118@gmail.com>
2020-07-21 15:07:14 -0700

    first commit!

---
+++ src/build/precache-manifest.87b29671c186df45fd731f605281e069.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "589370d453aa610457c7da4758513dc7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3cb779280699f585869",
+    "url": "/static/css/main.fe6824ef.chunk.css"
+  },
+  {
+    "revision": "d9234a30078daf0578e9",
+    "url": "/static/js/2.10e888b6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.10e888b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3cb779280699f585869",
+    "url": "/static/js/main.f25647d9.chunk.js"
+  },
+  {
+    "revision": "caf43f0a14314da69f12",
+    "url": "/static/js/runtime-main.226f7c93.js"
+  }
+]);
